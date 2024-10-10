7496ef85f47f6466ec97d6148a80060a4c3cca53
xturus138 <radityaramadhan.138@gmail.com>
2024-10-10 19:55:38 +0700

    Ui Fix

--- app/src/main/java/com/example/weatherapp/source/response/LocationProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.source.response
-
-import android.content.Context
-import android.location.LocationManager
-
-interface LocationProvider {
-    fun getLocationManager(): LocationManager
-    fun getContextForLocation(): Context
-}
